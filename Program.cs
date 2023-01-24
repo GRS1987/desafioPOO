081aed3e41b6e686d1a19b928c11b1fba3c6a017
Guilherme Rodrigo da Silva <guilhermerodrigodasilva@gmail.com>
2023-01-23 23:06:22 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "6569-5502", modelo:"NK-15", imei:"55555555", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "4589-2525", modelo:"Iphone 14", imei:"8888888888", memoria: 512);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Spotify");
+
